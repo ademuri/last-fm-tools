5a039c2d781acb9b52c0fecca243fd160471006b
Adam Demuri <adam.demuri@gmail.com>
2020-06-13 10:22:59 -0600

    Add full date string parsing to top-albums

--- cmd/topAlbums.go
+++ cmd/topAlbums.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
@@ -27,12 +28,12 @@ import (
 )
 
 var topAlbumsCmd = &cobra.Command{
-	Use:   "top-albums [date string]",
+	Use:   "top-albums [from] [to (optional)]",
 	Short: "Gets the user's top albums",
-	Long:  `TODO`,
-	Args:  cobra.ExactArgs(1),
+	Long:  `Uses the specified date or date range. Date strings look like 'yyyy', 'yyyy-mm', or 'yyyy-mm-dd'.`,
+	Args:  cobra.RangeArgs(1, 2),
 	Run: func(cmd *cobra.Command, args []string) {
-		err := getTopAlbums(viper.GetString("database"), args[0])
+		err := printTopAlbums(viper.GetString("database"), args)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -42,16 +43,6 @@ var topAlbumsCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(topAlbumsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// topAlbumsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// topAlbumsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 type AlbumCount struct {
@@ -60,25 +51,42 @@ type AlbumCount struct {
 	Count  int64
 }
 
-func getTopAlbums(dbPath string, ds string) error {
+type ParsedDate struct {
+	Date  time.Time
+	Year  bool
+	Month bool
+	Day   bool
+}
+
+func printTopAlbums(dbPath string, args []string) error {
+	var start, end time.Time
+	var err error
+	switch len(args) {
+	case 1:
+		start, end, err = getImplicitDateRange(args[0])
+
+	case 2:
+		start, end, err = getExplicitDateRange(args[0], args[1])
+
+	default:
+		return fmt.Errorf("Expected one or two date arguments")
+	}
+	if err != nil {
+		return fmt.Errorf("Parsing date range: %w", err)
+	}
+
 	db, err := openDb(dbPath)
 	if err != nil {
-		return fmt.Errorf("getTopAlbums: %w", err)
+		return fmt.Errorf("printTopAlbums: %w", err)
 	}
 
 	exists, err := dbExists(db)
 	if err != nil {
-		return fmt.Errorf("getTopAlbums: %w", err)
+		return fmt.Errorf("printTopAlbums: %w", err)
 	}
 	if !exists {
 		return fmt.Errorf("Database doesn't exist - run update first.")
 	}
-
-	start, err := time.Parse("2006-01", ds)
-	if err != nil {
-		return fmt.Errorf("getTopAlbums(%q): %w", ds, err)
-	}
-	end := start.AddDate(0, 1, 0)
 
 	user := strings.ToLower(viper.GetString("user"))
 
@@ -94,7 +102,7 @@ func getTopAlbums(dbPath string, ds string) error {
 	`
 	countQuery, err := db.Query(countQueryString, user, start.Unix(), end.Unix())
 	if err != nil {
-		return fmt.Errorf("getTopAlbums(%q): %w", ds, err)
+		return fmt.Errorf("printTopAlbums: %w", err)
 	}
 
 	n := 0
@@ -112,4 +120,94 @@ func getTopAlbums(dbPath string, ds string) error {
 	fmt.Printf("Found %d albums\n", n)
 
 	return nil
+}
+
+func getImplicitDateRange(ds string) (start time.Time, end time.Time, err error) {
+	date, err := parseSingleDatestring(ds)
+	if err != nil {
+		return
+	}
+
+	start = date.Date
+	switch {
+	case date.Year:
+		end = start.AddDate(1, 0, 0)
+
+	case date.Month:
+		end = start.AddDate(0, 1, 0)
+
+	case date.Day:
+		end = start.AddDate(0, 0, 1)
+
+	default:
+		err = fmt.Errorf("Invalid format: %q", ds)
+	}
+
+	return
+}
+
+func getExplicitDateRange(startString, endString string) (start time.Time, end time.Time, err error) {
+	startParsed, err := parseSingleDatestring(startString)
+	if err != nil {
+		return
+	}
+	start = startParsed.Date
+
+	endParsed, err := parseSingleDatestring(endString)
+	if err != nil {
+		return
+	}
+	end = endParsed.Date
+
+	return
+}
+
+func parseSingleDatestring(ds string) (date ParsedDate, err error) {
+	matched, err := regexp.Match(`^\d{4}$`, []byte(ds))
+	if err != nil {
+		err = fmt.Errorf("Parsing datestring as year: %w", err)
+		return
+	}
+	if matched {
+		date.Date, err = time.Parse("2006", ds)
+		if err != nil {
+			err = fmt.Errorf("Parsing datestring as year: %w", err)
+			return
+		}
+		date.Year = true
+		return
+	}
+
+	matched, err = regexp.Match(`^\d{4}-\d{2}$`, []byte(ds))
+	if err != nil {
+		err = fmt.Errorf("Parsing datestring as month: %w", err)
+		return
+	}
+	if matched {
+		date.Date, err = time.Parse("2006-01", ds)
+		if err != nil {
+			err = fmt.Errorf("Parsing datestring as month: %w", err)
+			return
+		}
+		date.Month = true
+		return
+	}
+
+	matched, err = regexp.Match(`^\d{4}-\d{2}-\d{2}$`, []byte(ds))
+	if err != nil {
+		err = fmt.Errorf("Parsing datestring as day: %w", err)
+		return
+	}
+	if matched {
+		date.Date, err = time.Parse("2006-01-02", ds)
+		if err != nil {
+			err = fmt.Errorf("Parsing datestring as day: %w", err)
+			return
+		}
+		date.Day = true
+		return
+	}
+
+	err = fmt.Errorf("Invalid format: %q", ds)
+	return
 }
