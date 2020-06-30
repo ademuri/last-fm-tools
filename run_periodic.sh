7666d9b71ae9b32168844003963f29a8fb23e52d
Adam Demuri <adam.demuri@gmail.com>
2020-06-29 19:23:40 -0600

    Add an example script for sending reports periodically

---
+++ run_periodic.sh
@@ -0,0 +1,22 @@
+set -euo pipefail
+
+# Send reports for these users to these email addresses (in order).
+users=("me")
+emails=("me@me.com")
+
+mkdir -p "$HOME/.last-fm-tools"
+cd "$HOME/.last-fm-tools"
+
+if [ ! -d "last-fm-tools" ]; then
+  git clone https://github.com/ademuri/last-fm-tools.git
+fi
+
+cd last-fm-tools
+git pull
+
+for i in ${!users[@]}; do
+  echo "Updating database for ${users[$i]}"
+  bazel run //:last-fm-tools -- update --user "${users[$i]}"
+  bazel run //:last-fm-tools -- email "${emails[$i]}" --user "${users[$i]}"
+done
+
