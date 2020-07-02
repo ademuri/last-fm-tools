484e93e495db05844495ce161aba04c5a516268b
Adam Demuri <adam.demuri@gmail.com>
2020-07-02 15:50:17 -0600

    Fix report logic to send report for this month

--- cmd/email.go
+++ cmd/email.go
@@ -95,7 +95,7 @@ func sendEmail(dbPath string, fromAddress string, dryRun bool, reportName string
 				fmt.Printf("Report was already sent this month on %s, not sending.\n", sent.Format("2006-01-02"))
 				return nil
 			}
-			if sent.After(toSendLastMonth) {
+			if now.After(toSendThisMonth) && sent.After(toSendLastMonth) {
 				fmt.Printf("Report was already sent for last month on %s, not sending.\n", sent.Format("2006-01-02"))
 				return nil
 			}
