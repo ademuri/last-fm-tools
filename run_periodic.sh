11e71f862296a4fb3f7772e2004b57ba5feb44bd
Adam DeMuri <adam.demuri@gmail.com>
2020-07-01 18:02:44 -0600

    Change the database to not live in the git repo

--- run_periodic.sh
+++ run_periodic.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 users=("me")
 emails=("me@me.com")
 
-mkdir -p "$HOME/.last-fm-tools"
-cd "$HOME/.last-fm-tools"
+tools_dir="$HOME/.last-fm-tools"
+mkdir -p "${tools_dir}"
+cd "${tools_dir}"
 
 if [ ! -d "last-fm-tools" ]; then
   git clone https://github.com/ademuri/last-fm-tools.git
@@ -16,8 +17,8 @@ git pull
 
 for i in ${!users[@]}; do
   echo "Updating database for ${users[$i]}"
-  bazel run //:last-fm-tools -- update --user "${users[$i]}" --database "${PWD}/lastfm.db"
-  bazel run //:last-fm-tools -- email "${emails[$i]}" top-artists new-artists --user "${users[$i]}" --database "${PWD}/lastfm.db" --name "Artists" --run_day 1
-  bazel run //:last-fm-tools -- email "${emails[$i]}" top-albums new-albums --user "${users[$i]}" --database "${PWD}/lastfm.db" --name "Albums" --run_day 15
+  bazel run //:last-fm-tools -- update --user "${users[$i]}" --database "${tools_dir}/lastfm.db"
+  bazel run //:last-fm-tools -- email "${emails[$i]}" top-artists new-artists --user "${users[$i]}" --database "${tools_dir}/lastfm.db" --name "Artists" --run_day 1
+  bazel run //:last-fm-tools -- email "${emails[$i]}" top-albums new-albums --user "${users[$i]}" --database "${tools_dir}/lastfm.db" --name "Albums" --run_day 15
 done
 
